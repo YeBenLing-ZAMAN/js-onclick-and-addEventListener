fd001d4151b8aa2e33a7467ff9476e14af541aea
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-02-08 18:46:40 +0600

    function calling but handing with id or class

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function makeRed(){
     document.body.style.backgroundColor="red";
     document.body.style.color="white";
 }
+
+
+// funcion calling but handling with id or class
+
+const yellowButton = document.getElementById("make-yellow");
+
+yellowButton.onclick = makeYellow;
+function makeYellow(){
+    document.body.style.backgroundColor="yellow";
+    document.body.style.color="black";
+}
+
