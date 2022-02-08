05cec7a2f8d4d036ad03c7b3320c63bed2e12286
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-02-08 18:58:39 +0600

    handling has anonymous function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // handling use function  
-function makeRed(){
-    document.body.style.backgroundColor="red";
-    document.body.style.color="white";
+function makeRed() {
+    document.body.style.backgroundColor = "red";
+    document.body.style.color = "white";
 }
 
 
@@ -10,17 +10,26 @@ function makeRed(){
 const yellowButton = document.getElementById("make-yellow");
 
 yellowButton.onclick = makeYellow; // () ekhane ami function ke call kore parbo na, jokhon button ta click hobe tokhon auto-maticlly call hoye jabe.
-function makeYellow(){
-    document.body.style.backgroundColor="yellow"; 
-    document.body.style.color="black";
+function makeYellow() {
+    document.body.style.backgroundColor = "yellow";
+    document.body.style.color = "black";
 }
 
 
 // handling by function 
 const pinkbutton = document.getElementById("make-pink");
 
-pinkbutton.onclick = function makepink (){
-    document.body.style.backgroundColor="pink"; 
-    document.body.style.color="white";
+pinkbutton.onclick = function makepink() {
+    document.body.style.backgroundColor = "pink";
+    document.body.style.color = "white";
+}
+
+// handling by anonymous funcrion 
+
+const blueButton = document.getElementById("make-blue");
+
+blueButton.onclick = function () { // function has no name
+    document.body.style.backgroundColor = "blue";
+    document.body.style.color = "white";
 }
 
